d51a14f7d610c53ea3d377198e727da66d8bed36
Federico Rossi <f.rossi57@studenti.unipi.it>
2018-11-03 16:45:22 +0100

    Added some tests

--- test/testOps.js
+++ test/testOps.js
@@ -2,7 +2,49 @@ var expect = require('chai').expect;
 var coapApi = require('../index.js');
 
 describe('testTest',() => {
-	it("Sould test the test",() => {
+	it("Should test the test",() => {
 		expect(true).to.equal(true);
+	});
+});
+
+describe('testGet',() => {
+	it("Should retrieve 'world'",done => {
+		coapApi.GET('coap.me','hello','')
+		.then(res => {
+			expect(res.status).to.equal('2.05');
+			expect(res.data).to.equal('world');
+			done();
+		})
+		.catch(err => {
+			done(err);
+		});
+	});
+});
+
+describe('testPost',() => {
+	it("Should retrieve 'POST OK'",done => {
+		coapApi.POST('coap.me','validate','','')
+		.then(res => {
+			expect(res.status).to.equal('2.01');
+			expect(res.data).to.equal('POST OK');
+			done();
+		})
+		.catch(err => {
+			done(err);
+		});
+	});
+});
+
+describe('testPut',() => {
+	it("Should retrieve 'PUT OK'",done => {
+		coapApi.PUT('coap.me','validate','','')
+		.then(res => {
+			expect(res.status).to.equal('2.04');
+			expect(res.data).to.equal('PUT OK');
+			done();
+		})
+		.catch(err => {
+			done(err);
+		});
 	});
 });
