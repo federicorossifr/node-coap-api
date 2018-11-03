14de65cea8fcce8a73ebb8f9cf008b4a8479cb15
Federico Rossi <f.rossi57@studenti.unipi.it>
2018-11-03 16:17:55 +0100

    Added testing

---
+++ test/testOps.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var coapApi = require('../index.js');
+
+describe('testTest',() => {
+	it("Sould test the test",() => {
+		expect(true).to.equal(true);
+	});
+});
